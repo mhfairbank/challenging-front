38beee519ce54a027461057a34ea6a96148912fb
mhfairbank <mhfairbank@gmail.com>
2014-12-24 16:17:52 -0600

    close to completing assessment

--- bounce-exchange/cart-test.js
+++ bounce-exchange/cart-test.js
@@ -1,9 +1,15 @@
+var siteString = $("html").html();
+siteString.match(/regexHere/g);
+
+
+
+
+"__olapicU=1419372932173; __utma=54652333.89895437.1419372714.1419372714.1419372714.1; __utmz=54652333.1419372714.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __sonar=11456480125256562132; ColeHaanNewsletterVisits=2; ColeHaanNewsletterSession=0; ga_vc=1; ga_login_sent=true; _gat=1; sid=FF_3UdgYS1SALdRI2Y854RUI2v7iWaOhBvA; dwanonymous_29c8bd709bce117811b14d53deb17581=acH88lcyIsLdaerGhREFQOSo7w; dwac_bct8giaagE38Yaaac663BfOTDo=FF_3UdgYS1SALdRI2Y854RUI2v7iWaOhBvA%3D|dw-only|||USD|false|America%2FNew%5FYork|true; tradeGlobalGeolocation=US; _gat_OlapicGoogleTracker=1; shoefitrUserId=o7MIGwEkS0SWDmdo3-4TBw2; _gali=sticky-header; mt.v=2.1840100747.1419373135610; dw=1; tmseen=1; __atuvc=186%7C52; __atuvs=549b019a25e13cc5081; __utma=161561134.1509080451.1419373138.1419441769.1419444635.4; __utmb=161561134.0.10.1419457322942; __utmc=161561134; __utmz=161561134.1419373138.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); smtrrmkr=635550541228953699%5E6da3ccae-f18a-e411-83a0-0a18bbb287c2%5Ee330aac6-a78b-e411-967c-0afd435a375a%5E0%5E; smtrsession=popupcheck%7C3%5E-SR-%7C!; ga_session=1419457323286; ga_registered_user=yes; ga_logged_in=yes; ga_purchaser_type=browser; _ga=GA1.2.1509080451.1419373138; __CT_Data=gpv=141&apv_12613_www07=141; WRUID=0"
+
+
+"__olapicU=1419372932173; __utma=54652333.89895437.1419372714.1419372714.1419372714.1; __utmz=54652333.1419372714.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); __sonar=11456480125256562132; ColeHaanNewsletterVisits=2; ColeHaanNewsletterSession=0; ga_vc=1; ga_login_sent=true; _gat=1; _gat_OlapicGoogleTracker=1; shoefitrUserId=o7MIGwEkS0SWDmdo3-4TBw2; __utma=161561134.1509080451.1419373138.1419441769.1419444635.4; __utmb=161561134.0.10.1419457322942; __utmc=161561134; __utmz=161561134.1419373138.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); sid=gt-PNtFblsWYqSpW3t8SVu7Lm0i8aYCr9cE; dwanonymous_29c8bd709bce117811b14d53deb17581=ab0Wye3N7vLRyOH0sLhkuvwUi3; tradeGlobalGeolocation=US; loginClick=login; _gali=dwfrm_login; dwac_bct8giaagE38Yaaac663BfOTDo=gt-PNtFblsWYqSpW3t8SVu7Lm0i8aYCr9cE%3D|dw-only|00521235||USD|false|America%2FNew%5FYork|true; tmseen=1; mt.v=2.1840100747.1419373135610; dw=1; __atuvc=190%7C52; __atuvs=549b019a25e13cc5085; ga_session=1419457411682; ga_registered_user=yes; ga_logged_in=yes; ga_purchaser_type=browser; smtrrmkr=635550542116713564%5E6da3ccae-f18a-e411-83a0-0a18bbb287c2%5Ee330aac6-a78b-e411-967c-0afd435a375a%5E0%5E; smtrsession=popupcheck%7C3%5E-SR-%7C!; _ga=GA1.2.1509080451.1419373138; __CT_Data=gpv=144&apv_12613_www07=144; WRUID=0"
+
 $('.mini-cart-total a')
-
-
-
-
-
 
 var scrollPercentage;
 $('#main').scroll(function(){
--- bounce-exchange/cart.js
+++ bounce-exchange/cart.js
@@ -1,3 +1,5 @@
+// Original Specs. Copy lines 3-25 into the console all at once. Be sure to add a few items to your cart on colehaan.com and return to the homepage first.
+
 var subtotal = $('.grid_6.value.omega').text();
 var itemCount = $('.mini-cart-quantity').text();
 var images = $('.mini-cart-image.alpha.grid_4 img');
@@ -18,6 +20,66 @@ $('#cartModal .modal-header a').after(' ');
 $(window).scroll(function(){
 	var scrollRatio = ($(window).scrollTop() + $(window).height()) / $(document).height();
 	if(scrollRatio >= 0.9){
+		$('#cartModal').modal();
+	}
+});
+
+
+
+
+
+// Only shows overlay when user is logged in. Doesn't use cookies. Copy lines 33-55 into the console all at once.
+
+var subtotal = $('.grid_6.value.omega').text();
+var itemCount = $('.mini-cart-quantity').text();
+var images = $('.mini-cart-image.alpha.grid_4 img');
+var cartURL = $('.mini-cart-total a').attr('href');
+var itemURLs = $('.mini-cart-name.grid_8 > a');
+
+$('#register-modal').after('<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModal" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="myModalLabel">Cart Subtotal: ' + subtotal + '<br />Item Count: ' + itemCount + '</h4><h3>Cart Items: </h3></div><div class="modal-footer"><a type="button" class="button btn btn-primary" role="button" href="' + cartURL + '" target="_blank">View Cart</a><br /><br /><button type="button" class="btn btn-default" data-dismiss="modal" style="width: auto;">Close</button></div></div></div></div>');
+
+for(var i = 0; i < images.size(); i++){
+	$('#cartModal .modal-header h3').after(itemURLs[i]);
+	$('#cartModal .modal-header a').first().text('');
+	$('#cartModal .modal-header a').first().append(images[i]);
+}
+
+$('#cartModal .modal-header a').attr('target','_blank');
+$('#cartModal .modal-header a').after(' ');
+
+$(window).scroll(function(){
+	var scrollRatio = ($(window).scrollTop() + $(window).height()) / $(document).height();
+	if(scrollRatio >= 0.9 && $('.my-account').size() > 0){
+		$('#cartModal').modal();
+	}
+});
+
+
+
+
+
+// Only shows overlay when user is logged in. Uses cookies. Copy lines 63-85 into the console all at once.
+
+var subtotal = $('.grid_6.value.omega').text();
+var itemCount = $('.mini-cart-quantity').text();
+var images = $('.mini-cart-image.alpha.grid_4 img');
+var cartURL = $('.mini-cart-total a').attr('href');
+var itemURLs = $('.mini-cart-name.grid_8 > a');
+
+$('#register-modal').after('<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModal" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="myModalLabel">Cart Subtotal: ' + subtotal + '<br />Item Count: ' + itemCount + '</h4><h3>Cart Items: </h3></div><div class="modal-footer"><a type="button" class="button btn btn-primary" role="button" href="' + cartURL + '" target="_blank">View Cart</a><br /><br /><button type="button" class="btn btn-default" data-dismiss="modal" style="width: auto;">Close</button></div></div></div></div>');
+
+for(var i = 0; i < images.size(); i++){
+	$('#cartModal .modal-header h3').after(itemURLs[i]);
+	$('#cartModal .modal-header a').first().text('');
+	$('#cartModal .modal-header a').first().append(images[i]);
+}
+
+$('#cartModal .modal-header a').attr('target','_blank');
+$('#cartModal .modal-header a').after(' ');
+
+$(window).scroll(function(){
+	var scrollRatio = ($(window).scrollTop() + $(window).height()) / $(document).height();
+	if(scrollRatio >= 0.9 && document.cookie.indexOf("loginClick=login") > -1){
 		$('#cartModal').modal();
 	}
 });
