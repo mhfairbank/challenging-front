6d945a4565c44ec2e82c31930b2e197e91adc31d
mhfairbank <mhfairbank@gmail.com>
2014-12-24 15:22:53 -0600

    make all links open in new tab

--- bounce-exchange/cart.js
+++ bounce-exchange/cart.js
@@ -4,7 +4,7 @@ var images = $('.mini-cart-image.alpha.grid_4 img');
 var cartURL = $('.mini-cart-total a').attr('href');
 var itemURLs = $('.mini-cart-name.grid_8 > a');
 
-$('#register-modal').after('<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModal" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="myModalLabel">Cart Subtotal: ' + subtotal + '<br />Item Count: ' + itemCount + '</h4><h3>Cart Items: </h3></div><div class="modal-footer"><a type="button" class="button btn btn-primary" role="button" href="' + cartURL + '">View Cart</a><br /><br /><button type="button" class="btn btn-default" data-dismiss="modal" style="width: auto;">Close</button></div></div></div></div>');
+$('#register-modal').after('<div class="modal fade" id="cartModal" tabindex="-1" role="dialog" aria-labelledby="cartModal" aria-hidden="true"><div class="modal-dialog modal-sm"><div class="modal-content"><div class="modal-header"><h4 class="modal-title" id="myModalLabel">Cart Subtotal: ' + subtotal + '<br />Item Count: ' + itemCount + '</h4><h3>Cart Items: </h3></div><div class="modal-footer"><a type="button" class="button btn btn-primary" role="button" href="' + cartURL + '" target="_blank">View Cart</a><br /><br /><button type="button" class="btn btn-default" data-dismiss="modal" style="width: auto;">Close</button></div></div></div></div>');
 
 for(var i = 0; i < images.size(); i++){
 	$('#cartModal .modal-header h3').after(itemURLs[i]);
